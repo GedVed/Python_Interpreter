3009e76bb0170e28f9b335b81f7aa22f1576da4b
rabobank <467001@student.fontys.nl>
2024-11-07 23:31:06 +0100

    Added a environmental variables to the Interpreter

--- interpreter.py
+++ interpreter.py
@@ -1,14 +1,37 @@
 
 
-class Interpreter:
+class Toy_Interpreter:
     def __init__(self):
         self.stack = []
+        self.environment = {} #holds env variables
 
     def add_to_stack(self, value):
-        self.stack.append(value)
+        self.stack.append(value) 
 
     def remove_from_stack(self):
-        self.stack.pop()
+        #somewhat redudant since pop already returns value, but since add_to_stack is there it stands to reason that remove should be there too
+        return self.stack.pop() 
+
+    def save_env_variable(self, variable_name):
+        variable_value = self.stack.pop()
+        self.environment[variable_name] = variable_value
+
+    def load_env_variable(self, variable_name):
+        variable_value = self.environment[variable_name]
+        self.add_to_stack(variable_value)
+
+    def parser(self, instruction, argument, execute):
+        #translation of arguments to instructions
+        variable_values = ["add_to_stack"]
+        variable_names = ["save_env_variable", "load_env_variable"]
+
+        if instruction in variable_values:
+            argument = execute["variable_values"][argument]
+        elif instruction in variable_names:
+            argument = execute["variable_names"][argument]
+    
+
+        return argument
 
     def addition(self):
         first_value = self.stack.pop()
@@ -22,18 +45,28 @@ class Interpreter:
     
     def run(self, execute):
         instructions = execute["instructions"]
-        values = execute["values"]
-        for step in instructions:
-            instruction, value = step
+        for execution_step in instructions:
+            instruction, argument = execution_step
+            argument = self.parser(instruction, argument, execute)
+            """
             if instruction == "add_to_stack":
-                value = values[value]
-                self.add_to_stack(value)
-            elif instruction == "remove_from_stack":
-                self.remove_from_stack()
+                self.add_to_stack(argument)
             elif instruction == "addition":
                 self.addition()
             elif instruction == "result":
                 self.result()
+            elif instruction == "save_env_variable":
+                self.save_env_variable(argument)
+            elif instruction == "load_env_variable":
+                self.load_env_variable(argument)
+            """
+            execution_method = getattr(self, instruction) #dynamic method lookup to avoid elif bloat
+            
+            if (execution_method is None):
+                execution_method()
+            else:
+                execution_method(argument)
+            
 
 
     
--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 
-from interpreter import Interpreter
+from interpreter import Toy_Interpreter
 
-byte_code_inter = Interpreter()
+byte_code_inter = Toy_Interpreter()
 
 
 sample_instructions = {
     "instructions": [
         ("add_to_stack", 0),
+        ("save_env_variable",0),
         ("add_to_stack", 1),
+        ("save_env_variable",1),
+        ("load_env_variable", 0),
+        ("load_env_variable", 1),
         ("addition", None),
-        ("result", None)
-        ],
-    "values": [5, 2]
+        ("result", None)],
+    "variable_values": [5, 2],
+    "variable_names": ["first_value", "second_value"]
 }
+
+"""
+Instructions above translate to:
+
+first_value = 5
+second_value = 2
+print(first_value+second+value)
+
+"""
+
 
 
 byte_code_inter.run(sample_instructions)
